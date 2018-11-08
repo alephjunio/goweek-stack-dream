8f67b82afda6ff104c0f18121144aebc2064eedd
cjualephjunio <alef.junio@cjusystems.com.br>
2018-11-08 10:37:37 -0200

    correção de like socket

--- web/src/pages/Timeline.js
+++ web/src/pages/Timeline.js
@@ -3,81 +3,75 @@ import api from '../services/api';
 import Tweet from '../components/Tweet';
 import socket from 'socket.io-client';
 
-
 import twitterLogo from '../twitter.svg';
 import './Timeline.css';
 
+require('dotenv').config();
 
-export default class Timeline extends Component {
-
+class Timeline extends Component {
   state = {
     tweets: [],
     newTweet: ''
-  }
+  };
 
-
-  async componentDidMount(){
+  async componentDidMount() {
     this.subscribeToEvents();
 
     const response = await api.get('tweets');
-    this.setState({tweets: response.data });
+
+    this.setState({ tweets: response.data });
+  }
+
+  subscribeToEvents = () => {
+    const io = socket(process.env.REACT_APP_BASE_URL);
+
+    io.on('tweet', data => {
+      this.setState({ tweets: [data, ...this.state.tweets] });
+    });
+    io.on('like', data => {
+      this.setState({
+        tweets: this.state.tweets.map(
+          tweet => (tweet._id === data._id ? data : tweet)
+        )
+      });
+    });
   }
 
   handleNewTweet = async e => {
-    if( e.keyCode !== 13 ) return;
+    if (e.keyCode !== 13) return;
 
     const content = this.state.newTweet;
     const author = localStorage.getItem('@GoTwitter:username');
 
-    await api.post('tweets', { content, author })
+    await api.post('tweets', { content, author });
 
-    this.setState({ newTweet:''});
-  }
+    this.setState({ newTweet: '' });
+  };
 
   handleInputChange = e => {
     this.setState({ newTweet: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className='timeline-wrapper'>
+        <img src={twitterLogo} height={24} alt='Twitter logo' />
+        <form>
+          <textarea
+            value={this.state.newTweet}
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleNewTweet}
+            placeholder='O que está acontecendo?'
+          />
+        </form>
+        <ul className='tweet-list'>
+          {this.state.tweets.map(tweet => (
+            <Tweet key={tweet._id} tweet={tweet} />
+          ))}
+        </ul>
+      </div>
+    );
   }
+}
 
-
-  subscribeToEvents = () => {
-    const io = socket("http://localhost:3000");
-
-    io.on('tweet', data => {
-      this.setState({ tweets: [data, ...this.state.tweets ]});
-    });
-
-
-    io.on('like', data => {
-      this.setState({
-        tweets: this.state.tweets.map(tweet =>
-          tweet._id === data._id ? data : tweet
-        )})
-      });
-
-    }
-
-
-
-    render() {
-      return (
-        <div className="timeline-wrapper">
-          <img height={24}  src={twitterLogo} alt="GoTwitter" />
-          <form>
-            <textarea
-              value={ this.state.newTweet }
-              onChange={ this.handleInputChange }
-              onKeyDown={ this.handleNewTweet }
-              placeholder='Oque está acontecendo'
-            />
-          </form>
-
-          <ul>
-            { this.state.tweets.map(tweet => (
-              <Tweet key={tweet._id} tweet={tweet} />
-            ))}
-          </ul>
-        </div>
-
-      );
-    }
-  }
+export default Timeline;
